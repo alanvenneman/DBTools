189c317a1343dcc0a499d4ffa8c46806f3d1a299
Alan Venneman <alawrencev@gmail.com>
2022-03-03 16:14:24 -0600

    Create date_fixer.py
    I think I was just trying to stretch the limits of a nested if then statement here. Also, I had to slice off the year from a full date field and populate it in another field.

---
+++ date_fixer.py
@@ -0,0 +1,45 @@
+import arcpy
+import os
+from datetime import datetime
+
+
+path_sde_connection = "C:\\Users\\GISAdmin\\AppData\\Roaming\\ESRI\\ArcGISPro\\Favorites\\Default.sde"
+utility_features = os.path.join(path_sde_connection, "ArcSDE.SDE.Utilities_SANITARY\\ArcSDE.SDE.sGravityMain")
+
+date_to_string = []
+rows = arcpy.da.SearchCursor(utility_features, "InstallDate")
+for row in rows:
+    datetimeVal = row[0]
+    if datetimeVal is not None:
+        formattedTime = datetime.strftime(datetimeVal, '%Y')
+        date_to_string.append(formattedTime)
+    else:
+        date_to_string.append('check year')
+        print('null year')
+
+# TODO put the year_slice inside an Update or Insert Cursor loop
+
+
+def year_slice(install, year):
+    if install != 'check year' and install != "#":
+        if install[-4:-2] == '20' or install[-4:-2] == '19':
+            if year != install[-4:]:
+                if year == '' or year == '#':
+                    print(install[-4:])
+                else:
+                    print("check {}".format(year))
+            elif len(year) == 4:
+                print(year)
+            else:
+                print("check {}".format(year))
+        elif len(year) == 4:
+            print(year)
+        else:
+            print("check {}".format(year))
+    elif len(year) == 4:
+        print(year)
+    else:
+        print("check {}".format(year))
+
+
+year_slice("", "")
